5a6d5919c73ac31b04f82c1a375f752852de1522
Odhran <mullano@tcd.ie>
2019-02-19 13:57:46 +0000

    odhran deleting clutter

--- main.py
+++ main.py
@@ -5,14 +5,4 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello, World!'
-<<<<<<< HEAD
-    //Comment by Fraser
-=======
 
-	
-//odhran commenting here
-<<<<<<< HEAD
-//Rory commenting here
-=======
->>>>>>> da1aaf2a3dd090bce7079547289587eb3db26032
->>>>>>> 0a96bd4c35869734acd6013a9c064b02820de030
