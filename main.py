a752c68c0e537c60591f8b16f6dc0d1235ad08a3
Odhran <mullano@tcd.ie>
2019-02-20 09:56:36 +0000

    added some python to make the website load

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-   return render_template('home.html')
+    return render_template('home.html')
+
+if __name__ == '__main__':
+   app.run(debug = True)
+
 
