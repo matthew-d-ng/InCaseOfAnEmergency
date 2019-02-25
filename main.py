412c7954ab3bf5adcdef3e5173d7a99b0a4ee7ae
Odhran <mullano@tcd.ie>
2019-02-25 17:31:16 +0000

    added basic update list

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('home.html')
+    earthQuakeList = ["USGSted: Prelim M5.5 Earthquake central Mid-Atlantic Ridge Feb-25 15:05 UTC", 
+                    "USGSted: Prelim M5.5 Earthquake Macquarie Island region Feb-24 06:00 UTC",
+                    "USGSted: Prelim M5.5 Earthquake southern Mid-Atlantic Ridge Feb-22 21:15 UTC",
+                    "USGSted: Prelim M5.5 Earthquake near the coast of Ecuador Feb-22 10:40 UTC",
+                    "USGSted: Prelim M7.5 Earthquake Peru-Ecuador border region Feb-22 10:17 UTC"]
+    return render_template('home.html', earthQuakeList = earthQuakeList)
 
 if __name__ == '__main__':
    app.run(debug = True)
-
 
