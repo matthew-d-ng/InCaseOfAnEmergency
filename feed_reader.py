d888d60b157939e345b875b3df046a76c40cf3a5
Odhran <mullano@tcd.ie>
2019-03-19 14:39:12 +0000

    moved stuff around and made realtime listings better

--- feed_reader.py
+++ feed_reader.py
@@ -1,17 +1,39 @@
 import time
 
-import atoma, requests
+from earthquake import Earthquake
+import atoma, requests, re
+
 
 FEED = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.atom"
 
-def get_latest_quakes():
+def parse_listing(listing):
+    """ Return Earthquake object """
 
-    latest_quakes_title_list = []
+    title = listing.title.value
+    magnitude = float(re.search(r"\d*\.\d+|\d+", title).group())
+    details = listing.summary.value
+
+    datetime = re.search(r"\d+-\d+-\d+ \d+:\d+:\d+", details).group().split(" ")
+    date = datetime[0]
+    time = datetime[1]
+
+    location = re.findall(r"(\d*\.\d+|\d+)&deg", details)
+    latitude = location[0]
+    longitude = location[1]
+
+    depth = re.findall(r"(\d*\.\d+|\d+) km", details)[0]
+
+    new_quake = Earthquake(title, magnitude, time, date, latitude, longitude, depth)
+    return new_quake
+
+def get_latest_quakes():
     response = requests.get(FEED)
     latest_quakes = atoma.parse_atom_bytes(response.content)
     latest_quakes_title_list = []
     for listing in latest_quakes.entries:
-        latest_quakes_title_list.append(listing.title.value)
+        eq = parse_listing(listing)
+        latest_quakes_title_list.append(eq.timestamp + " :" + eq.title)
 
     return latest_quakes_title_list
 
+get_latest_quakes()
